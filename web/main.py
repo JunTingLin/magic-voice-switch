d4c6ac653ec62abc2bc08e449d83f1c6f11dc5b4
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-06-06 12:55:31 +0800

    feat(#6): interact with models

--- web/main.py
+++ web/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, Request, File, UploadFile
+import os
+import random
+
+from fastapi import FastAPI, File, Request, UploadFile
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from pydub import AudioSegment
-import random
+from models.classify import classify_and_print_results
 
 app = FastAPI()
 
@@ -15,12 +17,19 @@ def read_root(request: Request):
 
 # @api
 @app.get("/api/state")
-def get_state():
+async def get_state():
+    result_json = classify_and_print_results(
+        model_path=os.path.join('templates', 'models', 'soundclassifier_with_metadata.tflite'), 
+        labels_path=os.path.join('templates', 'models', 'labels.txt'),
+        audio_path=os.path.join('templates', 'output.wav')
+    )
+    print(result_json)
+    # TODO 把 result 給抽離出來
     return {"state": random.choice([1, 2])}
 
 @app.post("/api/upload")
 async def upload_file(file: UploadFile = File(...)):
     contents = await file.read()
-    with open("output.wav", "wb") as f:
+    with open(os.path.join('templates', 'output.wav'), "wb") as f:
         f.write(contents)
     return {"filename": file.filename}
